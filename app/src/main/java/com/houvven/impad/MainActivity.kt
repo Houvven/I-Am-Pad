4fa974830a577a95bbe7cce033a0bafb671e94d9
houvven <houvven@gmail.com>
2024-10-09 15:23:54 +0800

    add github repo url

--- app/src/main/java/com/houvven/impad/MainActivity.kt
+++ app/src/main/java/com/houvven/impad/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.houvven.impad
 
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -19,23 +17,19 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Snackbar
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
-import androidx.compose.material3.SnackbarVisuals
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableLongStateOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -43,7 +37,6 @@ import androidx.compose.ui.unit.dp
 import androidx.core.graphics.drawable.toBitmap
 import com.highcapable.yukihookapi.hook.factory.dataChannel
 import com.houvven.impad.ui.theme.AppTheme
-import kotlinx.coroutines.launch
 
 private val snackbarHostState = SnackbarHostState()
 
@@ -90,10 +83,11 @@ private fun App(modifier: Modifier = Modifier) {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun AppTopBar() {
+    val uriHandler = LocalUriHandler.current
     TopAppBar(
         title = { Text(text = stringResource(R.string.app_name)) },
         actions = {
-            IconButton(onClick = {}) {
+            IconButton(onClick = { uriHandler.openUri("https://github.com/Houvven/I-Am-Pad") }) {
                 Image(
                     painter = painterResource(R.drawable.ic_github),
                     contentDescription = null,
