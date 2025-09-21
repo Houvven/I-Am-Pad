569a036383a7765f9fc8875661ba1774bad11dc5
houvven <houvven@gmail.com>
2025-09-21 14:24:18 +0800

    Upgrade version to 1.0.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = namespace
         minSdk = 24
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.0.5"
+        versionCode = 7
+        versionName = "1.0.6"
         vectorDrawables {
             useSupportLibrary = true
         }
