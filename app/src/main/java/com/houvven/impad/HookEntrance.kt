b615df7adf67a982372b9612c9a792441e5b1ed4
houvven <houvven@gmail.com>
2025-09-22 12:56:33 +0800

    fix WeChat and DingTalk hook time, support DingTalk play store version

--- app/src/main/java/com/houvven/impad/HookEntrance.kt
+++ app/src/main/java/com/houvven/impad/HookEntrance.kt
@@ -82,107 +82,93 @@ object HookEntrance : IYukiHookXposedInit {
         }
     }
 
-    private fun PackageParam.processWeChat() = onAppLifecycle {
-        attachBaseContext { context, hasCalledSuper ->
-            System.loadLibrary("dexkit")
-            val prefs = context.dexkitPrefs
-            val classLoader = context.classLoader
+    private fun PackageParam.processWeChat() = afterApplicationAttach { context ->
+        val prefs = context.dexkitPrefs
+        val classLoader = context.classLoader
 
-            findOrLoadMethod(prefs, "checkLoginAsPad_method", classLoader) {
-                findMethod {
-                    excludePackages("android", "androidx", "com")
-                    matcher {
-                        modifiers(Modifier.PUBLIC or Modifier.FINAL)
-                        paramCount(3)
-                        paramTypes(
-                            String::class.java,
-                            String::class.java,
-                            Continuation::class.java
-                        )
-                        usingStrings(
-                            "MicroMsg.CgiCheckLoginAsPad",
-                            "/cgi-bin/micromsg-bin/checkloginaspad"
-                        )
-                    }
-                }.single().toDexMethod()
-            }.hook().replaceToTrue()
-
-            findOrLoadMethod(prefs, "isFoldableDevice_method", classLoader) {
-                findMethod {
-                    searchPackages("com.tencent.mm.ui")
-                    matcher {
-                        modifiers(Modifier.PUBLIC or Modifier.STATIC)
-                        paramCount(0)
-                        usingStrings("royole", "tecno", "ro.os_foldable_screen_support")
-                        returnType(Boolean::class.javaPrimitiveType!!)
-                    }
-                }.single().toDexMethod()
-            }.hook().replaceToTrue()
-        }
-    }
-
-    private fun PackageParam.processWeWork() {
-        Application::class.resolve().firstMethod {
-            name("attach")
-        }.hook().after {
-            val context = args[0] as Context
-            val classLoader = context.classLoader
-            "com.tencent.wework.foundation.impl.WeworkServiceImpl".toClass(classLoader)
-                .resolve()
-                .method {
-                    name { it.startsWith("isAndroidPad") }
-                    returnType(Boolean::class)
+        findOrLoadMethod(prefs, "checkLoginAsPad_method", classLoader) {
+            findMethod {
+                excludePackages("android", "androidx", "com")
+                matcher {
+                    modifiers(Modifier.PUBLIC or Modifier.FINAL)
+                    paramCount(3)
+                    paramTypes(
+                        String::class.java,
+                        String::class.java,
+                        Continuation::class.java
+                    )
+                    usingStrings(
+                        "MicroMsg.CgiCheckLoginAsPad",
+                        "/cgi-bin/micromsg-bin/checkloginaspad"
+                    )
                 }
-                .hookAll().replaceToTrue()
-        }
+            }.single().toDexMethod()
+        }.hook().replaceToTrue()
+
+        findOrLoadMethod(prefs, "isFoldableDevice_method", classLoader) {
+            findMethod {
+                searchPackages("com.tencent.mm.ui")
+                matcher {
+                    modifiers(Modifier.PUBLIC or Modifier.STATIC)
+                    paramCount(0)
+                    usingStrings("royole", "tecno", "ro.os_foldable_screen_support")
+                    returnType(Boolean::class.javaPrimitiveType!!)
+                }
+            }.single().toDexMethod()
+        }.hook().replaceToTrue()
     }
 
-    private fun PackageParam.processDingTalk() = onAppLifecycle {
-        attachBaseContext { context, hasCalledSuper ->
-            System.loadLibrary("dexkit")
-            val classLoader = context.classLoader
-            val prefs = context.dexkitPrefs
-            val cacheKey = "isMultiLoginFoldableDevice_method"
+    private fun PackageParam.processWeWork() = afterApplicationAttach { context ->
+        val classLoader = context.classLoader
+        "com.tencent.wework.foundation.impl.WeworkServiceImpl".toClass(classLoader)
+            .resolve()
+            .method {
+                name { it.startsWith("isAndroidPad") }
+                returnType(Boolean::class)
+            }
+            .hookAll().replaceToTrue()
+    }
 
-            findOrLoadMethod(prefs, cacheKey, classLoader) {
-                findMethod {
-                    searchPackages("com.alibaba.android.dingtalkbase.foldable")
-                    matcher {
-                        modifiers(Modifier.STATIC)
-                        paramTypes(Activity::class.java)
-                        returnType(Boolean::class.javaPrimitiveType!!)
-                        usingStrings("isMultiLoginFoldableDevice")
-                        addUsingField { type("com.android.alibaba.ip.runtime.IpChange") }
-                    }
-                }.single().toDexMethod()
-            }.hook().replaceToTrue()
-        }
+    private fun PackageParam.processDingTalk() = afterApplicationAttach { context ->
+        val classLoader = context.classLoader
+        val prefs = context.dexkitPrefs
+        val cacheKey = "isMultiLoginFoldableDevice_method"
+
+        findOrLoadMethod(prefs, cacheKey, classLoader) {
+            findMethod {
+                searchPackages("com.alibaba.android.dingtalkbase.foldable")
+                matcher {
+                    modifiers(Modifier.STATIC)
+                    paramCount(1)
+                    paramTypes(Activity::class.java)
+                    returnType(Boolean::class.javaPrimitiveType!!)
+                    usingStrings("isMultiLoginFoldableDevice")
+                }
+            }.single().toDexMethod()
+        }.hook().replaceToTrue()
     }
 
     @SuppressLint("DuplicateCreateDexKit")
-    private fun PackageParam.processCustomWeWork() = onAppLifecycle {
-        attachBaseContext { context, hasCalledSuper ->
-            System.loadLibrary("dexkit")
-            val classLoader = context.classLoader
-            val prefs = context.dexkitPrefs
-            val cacheKey = "isPadJudge_method"
+    private fun PackageParam.processCustomWeWork() = afterApplicationAttach { context ->
+        val classLoader = context.classLoader
+        val prefs = context.dexkitPrefs
+        val cacheKey = "isPadJudge_method"
 
-            findOrLoadMethod(prefs, cacheKey, classLoader) {
-                findMethod {
-                    matcher {
-                        declaredClass("com.tencent.wework.common.utils.WwUtil")
-                        returnType(Boolean::class.javaPrimitiveType!!)
-                        paramCount(0)
-                        modifiers(Modifier.STATIC)
-                        usingStrings(
-                            "isPadJudge",
-                            "isPadWhiteListFromServer", "isPadBlackListFromServer",
-                            "isPadWhiteListFromLocal", "isPadBlackListFromLocal"
-                        )
-                    }
-                }.single().toDexMethod()
-            }.hook().replaceToTrue()
-        }
+        findOrLoadMethod(prefs, cacheKey, classLoader) {
+            findMethod {
+                matcher {
+                    declaredClass("com.tencent.wework.common.utils.WwUtil")
+                    returnType(Boolean::class.javaPrimitiveType!!)
+                    paramCount(0)
+                    modifiers(Modifier.STATIC)
+                    usingStrings(
+                        "isPadJudge",
+                        "isPadWhiteListFromServer", "isPadBlackListFromServer",
+                        "isPadWhiteListFromLocal", "isPadBlackListFromLocal"
+                    )
+                }
+            }.single().toDexMethod()
+        }.hook().replaceToTrue()
     }
 
     private fun PackageParam.processXhs() {
@@ -245,4 +231,13 @@ object HookEntrance : IYukiHookXposedInit {
 
     private fun isCustomWeWorkPackage(classLoader: ClassLoader?) =
         classLoader?.hasClass("com.tencent.wework.common.utils.WwUtil") == true
+
+    private fun PackageParam.afterApplicationAttach(action: (Context) -> Unit) {
+        Application::class.resolve().firstMethod {
+            name("attach")
+            parameters(Context::class)
+        }.hook().after {
+            action(args[0] as Context)
+        }
+    }
 }
