7e982a3c656cf1a005dab5486e2ecab66f8abff6
houvven <houvven@gmail.com>
2025-09-22 15:29:42 +0800

    shrinking apk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,7 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
